4c788a9c33c0c577b0a237bcdcca5cf42a5f8bac
s-oku <oku@realglobe.jp>
2019-06-19 19:39:55 +0900

    ACM Certificate

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_acm_certificate" "cert" {
+  provider = var.provider_alias
+  domain_name = var.domain_names[0]
+  subject_alternative_names = slice(var.domain_names, 1, length(var.domain_names))
+  validation_method = "DNS"
+
+  tags = {
+    Name = var.acm_cert_tag_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider = var.provider_alias
+  certificate_arn         = aws_acm_certificate_validation.cert.certificate_arn
+  validation_record_fqdns = aws_route53_record.cert_validation.*.fqdn
+}
+
+resource "aws_route53_record" "cert_validation" {
+  count   = length(var.domain_names)
+  name    = lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name")
+  type    = lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type")
+  records = [lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_value")]
+  zone_id = var.route53_zone_id
+  ttl     = 60
+
+  lifecycle {
+    ignore_changes = ["fqdn"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "provider_alias" {}
+variable "domain_names" {
+  type = list(string)
+}
+variable "route53_zone_id" {}
+variable "acm_cert_tag_name" {}
